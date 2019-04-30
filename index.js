5b2a24e1818868ad9e6317bbacfb8451aa6a8e51
Adam Straus <a.straus1@gmail.com>
2019-04-30 21:21:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 
-//function theBeatlesPlay(musicians, instruments){
+/*function theBeatlesPlay(musicians, instruments){
   var emptyArray[];
   for(i=0;i<musicians.length;i++)
   {
